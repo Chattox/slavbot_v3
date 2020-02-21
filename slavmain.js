f30eb13d5befe4e9df1bca9cc67b63c024ee33cd
Chattox <conradlineker@gmail.com>
2020-02-21 22:45:42 +0000

    Tidied up slavmain.js side of playSound

--- slavmain.js
+++ slavmain.js
@@ -1,5 +1,6 @@
 // It's slav time
 const Discord = require('discord.js');
+const fs = require('fs').promises;
 const client = new Discord.Client();
 const { prefix, TOKEN } = require('./config.json');
 const { playSound } = require('./slavsound');
@@ -15,9 +16,25 @@ client.on('message', async message => {
     return;
   }
 
+  // Check msg is valid command
   if (message.content.startsWith(prefix)) {
-    playSound(message.content.substring(1), message);
-    console.log(message.content.substring(1));
+    // Strip prefix from message to get command
+    const command = message.content.substring(1);
+
+    // Check if command is referencing a sound
+    fs.readdir('./sounds')
+      .then(soundList => {
+        // Strip '.mp3' from list of files in sounds folder to make into args for playSound
+        soundList.forEach((sound, index) => {
+          soundList[index] = sound.slice(0, -4);
+        });
+        if (soundList.includes(command)) {
+          playSound(command, message);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 });
 
