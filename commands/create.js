aa1d269878766ce4a74811ded60b0aac79d58857
Chattox <conradlineker@gmail.com>
2020-09-09 22:29:10 +0100

    begin separating util funcs into own files

--- commands/create.js
+++ commands/create.js
@@ -1,11 +1,11 @@
-const { createSoundManifest } = require('../slav_utils');
+const { createSoundManifest } = require('../utils/createSoundManifest');
 const { ADMIN_ID } = require('../config.json');
 
 const create = {
   name: 'create',
   description:
     'creates the sound_manifest.regularSounds array from filenames in the ./sounds directory',
-  execute: function(message) {
+  execute: function (message) {
     if (message.author.id === ADMIN_ID) {
       createSoundManifest();
     } else {
@@ -13,7 +13,7 @@ const create = {
       console.log('User is not admin');
       message.author.send('This command is for admins only, blyat');
     }
-  }
+  },
 };
 
 module.exports = create;
