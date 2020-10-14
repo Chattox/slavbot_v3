bb53da91c24726744558bf389a61f0ae99afebc8
Chattox <conradlineker@gmail.com>
2020-10-14 17:12:09 +0100

    add readuser command to check activities easier

---
+++ commands/readuser.js
@@ -0,0 +1,21 @@
+const { isAdmin } = require('../utils/isAdmin');
+
+const readuser = {
+  name: 'readuser',
+  description:
+    'Console logs the activities of the user object of the given ID. If no ID given, logs the author',
+  execute: function (message, args) {
+    if (isAdmin(message, true)) {
+      // check if args is an ID
+      if (typeof args[0] === 'string') {
+        console.log(
+          message.guild.members.cache.get(args.toString()).presence.activities
+        );
+      } else {
+        console.log(message.author.presence.activities);
+      }
+    }
+  },
+};
+
+module.exports = readuser;
