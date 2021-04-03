30da95f1eaea897001df9acbf1d5816e4a541d2b
Chattox <conradlineker@gmail.com>
2021-04-03 01:14:36 +0100

    fix: update cmds w/ args to err handle for no args passed

--- commands/regname.js
+++ commands/regname.js
@@ -1,28 +1,33 @@
 const soundManifest = require('../sound_manifest');
 const regUsers = require('../regular_users');
 const fs = require('fs').promises;
-const { isAdmin } = require('../utils');
+const { isAdmin } = require('../utils/isAdmin');
 const regname = {
   name: 'regname',
   description: 'updates the name property of existing regular user object',
   execute: function (message, args) {
     if (isAdmin(message.author.id, true)) {
-      if (regUsers[args[0]]) {
-        const oldName = regUsers[args[0]].name;
-        regUsers[args[0]].name = args[1];
-        const formattedRegUsers = JSON.stringify(regUsers);
-        fs.writeFile('./regular_users.json', formattedRegUsers, 'utf8')
-          .then((res) => {
-            console.log(
-              `User "${oldName}" updated to "${regUsers[args[0]].name}"!`
-            );
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+      if (args) {
+        if (regUsers[args[0]]) {
+          const oldName = regUsers[args[0]].name;
+          regUsers[args[0]].name = args[1];
+          const formattedRegUsers = JSON.stringify(regUsers);
+          fs.writeFile('./regular_users.json', formattedRegUsers, 'utf8')
+            .then((res) => {
+              console.log(
+                `User "${oldName}" updated to "${regUsers[args[0]].name}"!`
+              );
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          console.log('----------');
+          console.log(`User at ID ${args[0]} is not a regular user`);
+        }
       } else {
         console.log('----------');
-        console.log(`User at ID ${args[0]} is not a regular user`);
+        console.log('No arguments given!');
       }
     }
   },
--- commands/setrand.js
+++ commands/setrand.js
@@ -7,39 +7,44 @@ const setrand = {
   description: 'toggles whether a sound is random only or not',
   execute: function (message, args) {
     if (isAdmin(message.author.id, true)) {
-      if (soundManifest.regularSounds.includes(args[0])) {
-        soundManifest.regularSounds = soundManifest.regularSounds.filter(
-          (sound) => sound !== args[0]
-        );
-        soundManifest.randSounds.push(args[0]);
-        const jsonSoundManifest = JSON.stringify(soundManifest);
-        fs.writeFile('./sound_manifest.json', jsonSoundManifest, 'utf8')
-          .then((res) => {
-            console.log(`'${args[0]}' has been set to random only!`);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } else if (soundManifest.randSounds.includes(args[0])) {
-        soundManifest.randSounds = soundManifest.randSounds.filter(
-          (sound) => sound !== args[0]
-        );
-        soundManifest.regularSounds.push(args[0]);
-        const jsonSoundManifest = JSON.stringify(soundManifest);
-        fs.writeFile('./sound_manifest.json', jsonSoundManifest, 'utf8')
-          .then((res) => {
-            console.log(`'${args[0]}' has been removed from random only!`);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+      if (args) {
+        if (soundManifest.regularSounds.includes(args[0])) {
+          soundManifest.regularSounds = soundManifest.regularSounds.filter(
+            (sound) => sound !== args[0]
+          );
+          soundManifest.randSounds.push(args[0]);
+          const jsonSoundManifest = JSON.stringify(soundManifest);
+          fs.writeFile('./sound_manifest.json', jsonSoundManifest, 'utf8')
+            .then((res) => {
+              console.log(`'${args[0]}' has been set to random only!`);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else if (soundManifest.randSounds.includes(args[0])) {
+          soundManifest.randSounds = soundManifest.randSounds.filter(
+            (sound) => sound !== args[0]
+          );
+          soundManifest.regularSounds.push(args[0]);
+          const jsonSoundManifest = JSON.stringify(soundManifest);
+          fs.writeFile('./sound_manifest.json', jsonSoundManifest, 'utf8')
+            .then((res) => {
+              console.log(`'${args[0]}' has been removed from random only!`);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          console.log(
+            `'${args[0]}' was not found in either regular or random only sound lists!`
+          );
+          message.author.send(
+            `'${args[0]}' was not found in either regular or random only sound lists!`
+          );
+        }
       } else {
-        console.log(
-          `'${args[0]}' was not found in either regular or random only sound lists!`
-        );
-        message.author.send(
-          `'${args[0]}' was not found in either regular or random only sound lists!`
-        );
+        console.log('----------');
+        console.log('No arguments given!');
       }
     }
   },
