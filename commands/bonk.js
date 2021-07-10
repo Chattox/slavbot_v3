6c95b0de76f1cf443f384bff768c0702af35cae3
Chattox <conradlineker@gmail.com>
2021-07-10 17:15:20 +0100

    Add error handling for bonk command

--- commands/bonk.js
+++ commands/bonk.js
@@ -7,30 +7,43 @@ const bonk = {
   description: 'bonks a user into another channel',
   execute: function (message, args) {
     if (isAdmin(message.author.id, true)) {
+      if (args.length !== 2) {
+        console.log('Wrong amount of arguments! Check and try again');
+        return;
+      }
       const targetUserId = args[0];
       const destinationName = args[1].replace(/_/g, ' ');
 
       // Get user
-      let user = {};
+      let user;
       user = message.guild.members.cache.get(targetUserId);
+      if (!user) {
+        console.log('----------');
+        console.log('User not found!');
+        return;
+      }
 
       // Get destination channel
-      let destination = {};
+      let destination;
       message.guild.channels.cache.each((channel) => {
         if (channel.name.toLowerCase() === destinationName) {
           destination = channel;
         }
       });
+      if (!destination) {
+        console.log('----------');
+        console.log('Channel not found!');
+        return;
+      }
 
       console.log('----------');
-      console.log(user.voice.channel.name);
       console.log(`Bonking ${user.displayName} to ${destination.name}...`);
       playSound('bonk', user.voice.channel)
         .then((dispatcher) => {
           dispatcher.on('finish', () => {
             setTimeout(() => {
               user.voice.setChannel(destination);
-            }, 500);
+            }, 100);
           });
         })
         .catch((err) => {
