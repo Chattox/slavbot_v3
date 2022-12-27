ff2fb9f079bff29eeacbd7392e17d357eb0486e4
Chattox <conradlineker@gmail.com>
2022-12-27 21:40:17 +0000

    feat(logging): migrate slavcount logging from console to logger

--- commands/slavcount.js
+++ commands/slavcount.js
@@ -1,13 +1,21 @@
 const soundManifest = require('../sound_manifest.json');
+const { infoLogCtx } = require('../utils/loggingContextHelpers');
 
 const slavcount = {
   name: 'slavcount',
   description: 'Posts total number of slavsounds in chat',
   execute: function (message, args, logger) {
+    const logInfo = [
+      'utility',
+      message.author,
+      'slavcount',
+      message.channel,
+      args,
+    ];
     let count = 0;
     count += soundManifest.regularSounds.length;
     count += soundManifest.randsounds.length;
-    console.log(`Total: ${count}`);
+    logger.info(`Total slav sounds: ${count}`, infoLogCtx(...logInfo));
     message.channel.send(
       `There are a total of ${count} slavsounds.\n ${soundManifest.regularSounds.length} regular sounds\n ${soundManifest.randsounds.length} random only sounds`
     );
