2ef09146304322231d8e9b0e888d2707c29f7983
Chattox <conradlineker@gmail.com>
2022-12-27 20:08:10 +0000

    feat(logging): migrate readuser logging from console to logger

--- commands/readuser.js
+++ commands/readuser.js
@@ -1,19 +1,31 @@
 const { isAdmin } = require('../utils/isAdmin');
+const { infoLogCtx } = require('../utils/loggingContextHelpers');
 
 const readuser = {
   name: 'readuser',
   description:
     'Console logs the activities of the user object of the given ID. If no ID given, logs the author',
-  execute: function (message, args) {
+  execute: function (message, args, logger) {
     if (isAdmin(message.author.id, true)) {
+      const logInfo = [
+        'utility',
+        message.author,
+        'readuser',
+        message.channel,
+        args,
+      ];
       // check if args is an ID
       if (args && typeof args[0] === 'string') {
         console.log(
-          message.guild.members.cache.get(args.toString()).presence.activities
+          message.guild.members.cache.get(args.toString()).presence?.activities
         );
       } else {
-        console.log(message.author.presence.activities);
+        console.log(message.author.presence?.activities);
       }
+      logger.info(
+        'This is a test function. Testy test test',
+        infoLogCtx(...logInfo)
+      );
     }
   },
 };
