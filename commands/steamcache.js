bd4f20169df0216ca23fd3507b717d469755528b
Chattox <conradlineker@gmail.com>
2022-12-27 21:45:47 +0000

    feat(logging): migrate steamcache logging from console to logger

--- commands/steamcache.js
+++ commands/steamcache.js
@@ -1,6 +1,11 @@
 const { isAdmin } = require('../utils/isAdmin');
 const fs = require('fs').promises;
 const axios = require('axios').default;
+const {
+  infoLogCtx,
+  warnLogCtx,
+  errLogCtx,
+} = require('../utils/loggingContextHelpers');
 
 // Download Steam's applist and save to file, overwriting if already exists.
 
@@ -10,17 +15,30 @@ const steamcache = {
     'download steam applist and save to file, overwriting if already exists',
   execute: function (message, args, logger) {
     if (isAdmin(message.author.id, true)) {
+      const logInfo = [
+        'utility',
+        message.author,
+        'steamcache',
+        message.channel,
+        args,
+      ];
       const appListUrl =
         'https://api.steampowered.com/ISteamApps/GetAppList/v0001/';
       axios.get(appListUrl).then((res) => {
-        console.log('Steam applist downloaded, saving to file...');
+        logger.info(
+          'Steam applist downloaded, saving to file',
+          infoLogCtx(...logInfo)
+        );
         const jsonSteamList = JSON.stringify(res.data.applist.apps.app);
         fs.writeFile(__dirname + '/../steam_cache.json', jsonSteamList, 'utf8')
           .then((res) => {
-            console.log('steam_cache.json written!');
+            logger.info('steam_cache.json written', infoLogCtx(...logInfo));
           })
           .catch((err) => {
-            console.log(err);
+            logger.error(
+              'Error writing to steam_cache.json',
+              errLogCtx(...logInfo, err)
+            );
           });
       });
     }
